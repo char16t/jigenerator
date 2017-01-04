41c12c46418290acfafe004eec7bee2b601ec415
Valeriy Manenkov <v@manenkov.com>
2017-01-04 19:59:05 +0300

    Add Lexer class

---
+++ src/main/java/Lexer.java
@@ -0,0 +1,31 @@
+public class Lexer {
+    String text;
+    Integer pos;
+    Character currentChar;
+
+    public Lexer(String text) {
+        this.text = text;
+        this.pos = 0;
+        this.currentChar = this.text.charAt(this.pos);
+    }
+
+    public void error() throws Exception {
+        throw new Exception("Invalid character");
+    }
+
+    public void advance() {
+        this.pos += 1;
+        if (this.pos > this.text.length() - 1) {
+            this.currentChar = null;
+        } else {
+            this.currentChar = this.text.charAt(this.pos);
+        }
+    }
+
+    public void skipWhitespace() {
+        while (this.currentChar != null && Character.isSpaceChar(this.currentChar)) {
+            this.advance();
+        }
+    }
+
+}
