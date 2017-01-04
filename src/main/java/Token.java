6828122326c6a0e21f7d4174812b51845fec67c1
Valeriy Manenkov <v@manenkov.com>
2017-01-04 20:19:42 +0300

    Add Token class

---
+++ src/main/java/Token.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 1/4/17.
+ */
+public class Token {
+    TokenType type;
+    String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
