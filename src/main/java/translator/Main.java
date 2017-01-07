a161998fb08160e8b3aa0521e41b2efa62b75f48
Valeriy Manenkov <v@manenkov.com>
2017-01-07 19:33:30 +0300

    New test for future improvements

--- src/main/java/translator/Main.java
+++ src/main/java/translator/Main.java
@@ -15,6 +15,20 @@ public class Main {
                         "term   := factor *((MUL | DIV) *(factor));\n" +
                         "factor := (PLUS | MINUS) factor | INTEGER | LPAREN expr RPAREN;";
 
+        String futureSource1 =
+                "expr   := term *((PLUS | MINUS) | term);\n" +
+                "term   := factor *((MUL | DIV) *(factor));\n" +
+                "factor := (PLUS | MINUS) factor | INTEGER | LPAREN expr RPAREN;\n" +
+                "\n" +
+                "PLUS := '+';\n" +
+                "MINUS := '-';\n" +
+                "MUL := '*';\n" +
+                "DIV := '/';\n" +
+                "EQ := ':='\n" +
+                "LPAREN := '(';\n" +
+                "RPAREN := ')';\n" +
+                "INTEGER := (0|1|2|3|4|5|6|7|8|9) *(0|1|2|3|4|5|6|7|8|9);";
+
         Lexer lexer = new Lexer(source3);
         Parser parser = new Parser(lexer);
         Interpreter interpreter = new Interpreter(parser);
