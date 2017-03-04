472fc54e016bb2a7b517015d4b348361a206b272
Valeriy Manenkov <v.manenkov@gmail.com>
2017-03-04 18:56:02 +0300

    Add copyright comment

--- src/test/java/translator/MainTest.java
+++ src/test/java/translator/MainTest.java
@@ -24,7 +24,6 @@
 package translator;
 
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,11 +37,11 @@ import java.nio.file.Path;
  */
 public class MainTest {
     @Test
-    public void execTest1() throws Exception {
+    public void execTest() throws Exception {
         try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            final Path outputDir = Files.createTempDirectory("aaa_");
-            final String inputFile = this.getClass().getResource("/translator/source9.grammar").getPath();
-            new Main(System.out, inputFile, outputDir.toString()).exec();
+            final Path directory = Files.createTempDirectory("aaa_");
+            final String file = this.getClass().getResource("/translator/source9.grammar").getPath();
+            new Main(System.out, file, directory.toString()).exec();
             // TODO: Check generated sources
         }
     }
